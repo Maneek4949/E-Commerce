aa7419e1ddd864ea5b825278d05e6e706c1eddb4
Manoj Kumar Prajapati <69102431+Maneek4949@users.noreply.github.com>
2024-08-18 13:49:21 +0530

    fix footer and cart section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = () => {
             <Col md={3} sm={5} className='box'>
               <div className="logo">
                   <ion-icon name="bag"></ion-icon>
-                  <h1>Multimart</h1>
+                  <h1>shophub</h1>
               </div>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
             </Col>
             <Col md={3} sm={5} className='box'>
               <h2>About Us</h2>
               <ul>
-                <li>Careers</li>
+                <li>Carrer</li>
                 <li>Our Stores</li>
-                <li>Our Cares</li>
+                <li>Contact</li>
                 <li>Terms & Conditions</li>
                 <li>Privacy Policy</li>
               </ul>
@@ -37,9 +37,9 @@ const Footer = () => {
             <Col md={3} sm={5} className='box'>
               <h2>Contact Us</h2>
               <ul>
-                <li>70 Washington Square South, New York, NY 10012, United States </li>
-                <li>Email: uilib.help@gmail.com</li>
-                <li>Phone: +1 1123 456 780</li>
+                <li>New York, NY 10012, United States </li>
+                <li>Email: shophub@gmail.com</li>
+                <li>Phone: +1 256 235 094</li>
               </ul>
             </Col>
           </Row>
